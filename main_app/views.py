ca1395c685f43d06e055403b5ad9f808a5f06f88
jadewang425 <jadewang425@gmail.com>
2023-09-12 09:11:20 -0400

    part 2 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from .models import Finch
+
 # data for testing purpose
-finches = [
-  {'breed': 'American Goldfinch', 'description': 'Breeding males are bright yellow with black forehead, black wings with white markings', 'origin': 'North America'},
-  {'breed': 'Eurasian Bullfinch', 'description': 'The Eurasian bullfinch is a bulky bull-headed bird. The upper parts are grey; the flight feathers and short thick bill are black', 'origin': "Europe and Asia"},
-]
+# finches = [
+#   {'breed': 'American Goldfinch', 'description': 'Breeding males are bright yellow with black forehead, black wings with white markings', 'origin': 'North America'},
+#   {'breed': 'Eurasian Bullfinch', 'description': 'The Eurasian bullfinch is a bulky bull-headed bird. The upper parts are grey; the flight feathers and short thick bill are black', 'origin': "Europe and Asia"},
+# ]
 
 # Create your views here.
 def home(request):
@@ -14,4 +16,9 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
