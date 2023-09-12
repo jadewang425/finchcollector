e501783f32f1ec08ef9aac4a79cf3c3cc80950da
jadewang425 <jadewang425@gmail.com>
2023-09-12 09:11:20 -0400

    part 1 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+# data for testing purpose
+finches = [
+  {'breed': 'American Goldfinch', 'description': 'Breeding males are bright yellow with black forehead, black wings with white markings', 'origin': 'North America'},
+  {'breed': 'Eurasian Bullfinch', 'description': 'The Eurasian bullfinch is a bulky bull-headed bird. The upper parts are grey; the flight feathers and short thick bill are black', 'origin': "Europe and Asia"},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -8,4 +14,4 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-    pass
+    return render(request, 'finches/index.html', {'finches': finches})
